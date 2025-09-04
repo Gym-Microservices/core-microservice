0fae0600c75c9ca166c663f6b7b7f337db1f0a2c
jdColonia <JuanDavidColoniaAldana@gmail.com>
2025-09-04 12:24:37 -0500

    feat: add security and OpenAPI configurations, including Keycloak role converter

---
+++ src/main/java/com/gym/core/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package com.gym.core.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+        @Bean
+        public OpenAPI customOpenAPI(
+                @Value("${spring.application.name:Gym API}") String applicationName,
+                @Value("${server.port:8080}") String serverPort) {
+                return new OpenAPI()
+                                .info(new Info()
+                                                .title(applicationName + " - API")
+                                                .description("REST API for gym management with microservices")
+                                                .version("1.0.0"))
+                                .servers(List.of(
+                                                new Server().url("http://localhost:" + serverPort)
+                                                                .description("Local Development Server")));
+        }
+}
