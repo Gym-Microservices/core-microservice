73388a3c96606b32f668c463838f3479d9c956d1
jdColonia <JuanDavidColoniaAldana@gmail.com>
2025-09-08 00:30:23 -0500

    fix: improve OpenAPI configuration to include JWT authentication and user roles

--- src/main/java/com/gym/core/config/OpenApiConfig.java
+++ src/main/java/com/gym/core/config/OpenApiConfig.java
@@ -3,6 +3,9 @@ package com.gym.core.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,10 +21,20 @@ public class OpenApiConfig {
                 return new OpenAPI()
                                 .info(new Info()
                                                 .title(applicationName + " - API")
-                                                .description("REST API for gym management with microservices")
+                                                .description("REST API for gym management with microservices. " +
+                                                                "Uses Bearer JWT authentication. " +
+                                                                "Available roles: ROLE_ADMIN, ROLE_TRAINER, ROLE_MEMBER")
                                                 .version("1.0.0"))
                                 .servers(List.of(
                                                 new Server().url("http://localhost:" + serverPort)
-                                                                .description("Local Development Server")));
+                                                                .description("Local Development Server")))
+                                .components(new Components()
+                                                .addSecuritySchemes("bearer-key",
+                                                                new SecurityScheme()
+                                                                                .type(SecurityScheme.Type.HTTP)
+                                                                                .scheme("bearer")
+                                                                                .bearerFormat("JWT")
+                                                                                .description("JWT Bearer token for authentication")))
+                                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
         }
 }
